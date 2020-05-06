22ee7a88a7385fba14def945c8725ceff327f268
Antony Konstantinidis <ak@digitalessence.de>
2020-05-06 14:47:05 +0200

    create a navigation component

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li><a class="active" href="#books">Books</a></li>
+    <li><a href="#about">About</a></li>
+  </ul>
+</template>
+
+<style lang="scss">
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+
+    li {
+      float: left;
+    }
+
+    a {
+      display: block;
+      color: white;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+
+      &:hover:not(.active) {
+        background-color: #111;
+      }
+
+      &.active {
+        background-color: #4CAF50;
+      }
+    }
+  }
+</style>
