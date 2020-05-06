4b791ea8451dd7901577c8081eccab8b5d0f2906
Antony Konstantinidis <ak@digitalessence.de>
2020-05-06 22:23:23 +0200

    Add a watcher

--- src/components/MouseCursor.vue
+++ src/components/MouseCursor.vue
@@ -20,6 +20,19 @@ export default {
       y: 4,
     };
   },
+  watch: {
+    x(val) {
+      if (val > 150) {
+        alert('X is greater than 150');
+      }
+    },
+    y(newValue, oldValue) {
+      if (oldValue > 300 && newValue < oldValue) {
+        alert('Y ftw!');
+        this.y = 0;
+      }
+    },
+  },
   methods: {
     updatePosition(e) {
       this.x = e.offsetX;
