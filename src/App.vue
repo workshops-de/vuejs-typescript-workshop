d1104005b08c692a6e9963971c31f2c353675777
Antony Konstantinidis <ak@digitalessence.de>
2020-05-07 09:34:54 +0200

    Create an InfoBox component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <AppNavigation />
     <MouseCursor />
+    <InfoBox />
   </div>
 </template>
 
 <script>
 import AppNavigation from '@/components/AppNavigation.vue';
 import MouseCursor from '@/components/MouseCursor.vue';
+import InfoBox from '@/components/InfoBox.vue';
 
 export default {
   name: 'App',
   components: {
     AppNavigation,
     MouseCursor,
+    InfoBox,
   },
 };
 </script>
---
+++ src/components/InfoBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="isVisible = !isVisible">
+      <span v-if="isVisible">Hide</span>
+      <span v-else>Show</span>
+    </button>
+    <div
+      class="container"
+      v-show="isVisible"
+    >
+      Lorem ipsum.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoBox',
+  data() {
+    return {
+      isVisible: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+  .container {
+    border: 1px solid;
+    background-color: lightgrey;
+  }
+</style>
