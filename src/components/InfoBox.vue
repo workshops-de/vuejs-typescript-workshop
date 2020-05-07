4f19f8f9b33881a219ec799f4f154ad019e6eaad
Antony Konstantinidis <ak@digitalessence.de>
2020-05-07 12:38:35 +0200

    Create different colored boxes

--- src/components/InfoBox.vue
+++ src/components/InfoBox.vue
@@ -8,7 +8,9 @@
       class="container"
       v-show="isVisible"
     >
-      Lorem ipsum.
+      <div :class="['box', blueClass]"></div>
+      <div :class="{box: true, red: isRed}"></div>
+      <div class="box" :class="borderedBox" @click="update"></div>
     </div>
   </div>
 </template>
@@ -19,14 +21,55 @@ export default {
   data() {
     return {
       isVisible: true,
+      blueClass: 'blue',
+      isRed: true,
+      clicked: false,
     };
+  },
+  methods: {
+    update() {
+      this.clicked = true;
+    },
+  },
+  computed: {
+    borderedBox() {
+      return {
+        green: !this.clicked,
+        border: !this.clicked,
+        blue: this.clicked,
+      };
+    },
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .container {
     border: 1px solid;
     background-color: lightgrey;
+  }
+
+  .box {
+    width: 50px;
+    height: 50px;
+    display: inline-block;
+    background-color: gray;
+    border: 3px solid transparent;
+  }
+
+  .red {
+    background-color: red;
+  }
+
+  .blue {
+    background-color: blue;
+  }
+
+  .green {
+    background-color: green;
+  }
+
+  .border {
+    border: 3px solid black;
   }
 </style>
