9ea0c5fd84d7b3e5b605e44b3ed98048f69a4409
Antony Konstantinidis <ak@digitalessence.de>
2020-05-12 10:01:34 +0200

    Extend the BookEdit Component

--- src/views/BookEdit.vue
+++ src/views/BookEdit.vue
@@ -1,21 +1,75 @@
 <template>
-  <form>
-    <input
-      type="text"
-      v-model.lazy.trim="book.title"
-      name="title"
-    />
+  <form
+    novalidate
+    @submit.prevent="submit"
+  >
+    <p>
+      <label for="title">Title</label>
+      <input
+        type="text"
+        v-model="book.title"
+        name="title"
+        id="title"
+        @change="validateTitle"
+        :class="{'is-invalid': errors.title}"
+      />
+      <span v-if="errors.title">{{ errors.title }}</span>
+    </p>
+    <p>
+      <label for="abstract">Abstract</label>
+      <textarea
+        cols="50"
+        rows="10"
+        v-model="book.abstract"
+        name="abstract"
+        id="abstract"
+        @change="validateAbstract"
+        :class="{'is-invalid': errors.abstract}"
+      ></textarea>
+      <span v-if="errors.abstract">{{ errors.abstract }}</span>
+    </p>
+    <p>
+      <label for="author">Author</label>
+      <input
+        type="text"
+        v-model="book.author"
+        name="author"
+        id="author"
+        @change="validateAuthor"
+        :class="{'is-invalid': errors.author}"
+      />
+      <span v-if="errors.author">{{ errors.author }}</span>
+    </p>
+    <p>
+      <button
+        type="submit"
+        :disabled="isInvalid"
+      >Submit</button>
+    </p>
   </form>
 </template>
 
 <script>
+const required = (val) => val.length > 0;
+const minLength = (val, length) => val.length >= length;
+
 export default {
   name: 'BookEdit',
   data() {
     return {
       book: {},
       isbn: null,
+      errors: {
+        title: '',
+        abstract: '',
+        author: '',
+      },
     };
+  },
+  computed: {
+    isInvalid() {
+      return Object.keys(this.errors).some((val) => this.errors[val] !== '');
+    },
   },
   methods: {
     async getBook() {
@@ -25,6 +79,34 @@ export default {
     init(isbn) {
       this.isbn = isbn;
       this.getBook();
+    },
+    submit() {
+      console.log(this.book);
+    },
+    validateTitle() {
+      if (!required(this.book.title)) {
+        this.errors.title = 'Title is required.';
+      } else if (!minLength(this.book.title, 5)) {
+        this.errors.title = 'The title must be at least 5 characters long.';
+      } else {
+        this.errors.title = '';
+      }
+    },
+    validateAbstract() {
+      if (!required(this.book.abstract)) {
+        this.errors.abstract = 'Abstract is required.';
+      } else if (!minLength(this.book.abstract, 12)) {
+        this.errors.abstract = 'The abstract must be at least 12 characters long.';
+      } else {
+        this.errors.abstract = '';
+      }
+    },
+    validateAuthor() {
+      if (!required(this.book.author)) {
+        this.errors.author = 'Author is required.';
+      } else {
+        this.errors.author = '';
+      }
     },
   },
   created() {
@@ -36,3 +118,18 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  label {
+    margin-right: 5px;
+  }
+
+  span {
+    display: block;
+  }
+
+  .is-invalid {
+    border: 1px solid red;
+    color: red;
+  }
+</style>
